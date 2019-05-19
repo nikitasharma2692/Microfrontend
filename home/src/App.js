dfe3e72737e1ee935150381f927c1722f70c24da
Nikita Sharma <nikita_sharma-ggn@mckinsey.com>
2019-05-19 16:34:31 +0530

    Adding home app

---
+++ home/src/App.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h2>Welcome to HomePage</h2>
+        </div>
+        <p className="App-intro">
+          I'm Home Page
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
